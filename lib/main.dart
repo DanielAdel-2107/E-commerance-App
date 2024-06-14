1850df7403c1931da8b6a30256cfe0581bcd23c6
Daniel Adel <168005559+DanielAdel-2107@users.noreply.github.com>
2024-06-14 12:04:19 +0300

    env created done

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
+import 'package:e_commerance_app/core/app/env.variable.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  EnvVariable.instance.init(envType: EnvTypeEnum.dev);
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -15,6 +21,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: EnvVariable.instance.checkMde,
+      home: Scaffold(),
+    );
   }
 }
