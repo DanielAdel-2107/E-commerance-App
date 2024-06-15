3febb52c3c7ae63c6ffdd62ea0edf903ab9d39b6
Daniel Adel <168005559+DanielAdel-2107@users.noreply.github.com>
2024-06-15 21:11:39 +0300

    Font Weight done

---
+++ lib/core/style/fonts/font_family_helper.dart
@@ -0,0 +1,7 @@
+class FontFamilyHelper {
+  FontFamilyHelper._();
+  static const String cairoArabic = 'Cairo';
+  static const String pPoppinsEnglish = 'Poppins';
+
+  //TODO swith arabic font and english font
+}
